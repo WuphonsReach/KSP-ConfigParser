e8f2434b280f62470ac573ad5bd3d89c6131883b
Unknown <wuphonsreach@gmail.com>
2018-07-07 18:45:47 -0400

    setting up overall tests

---
+++ test/parse.Tests/ParserTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace parse.Tests
+{
+    public class ParserTests
+    {
+        public class MMCacheSimpleExampleTests : ParserTests
+        {
+            private const string _fileName = "MMCacheSimpleExample.cfg";
+
+            
+        }
+    }
+}
