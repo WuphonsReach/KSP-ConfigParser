dfa20b9273cd6c6515393bba6d19321b119db799
Unknown <wuphonsreach@gmail.com>
2018-07-07 19:41:35 -0400

    start filling out ParseConfigFile() method

--- src/parse/Parser.cs
+++ src/parse/Parser.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.IO;
 using parse.Models;
 
 namespace parse
 {
     public class Parser
     {
-        public ConfigFile ParseConfigFile(string filePath)
+        public ConfigFile ParseConfigFile(string filePath, Stream stream)
         {
-            var result = new ConfigFile();
+            var result = new ConfigFile
+            {
+                FilePath = filePath,
+            };
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                }
+            }
+
             return result;
         }
     }
