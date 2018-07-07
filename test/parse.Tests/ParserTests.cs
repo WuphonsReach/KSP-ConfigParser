ab3f5e4e7f693127e822f0f1bc4a7c2648d05172
Unknown <wuphonsreach@gmail.com>
2018-07-07 19:16:51 -0400

    Get all test files wired as embedded, test setup

--- test/parse.Tests/ParserTests.cs
+++ test/parse.Tests/ParserTests.cs
@@ -1,15 +1,51 @@
 using System;
+using System.IO;
+using System.Reflection;
 using Xunit;
 
 namespace parse.Tests
 {
     public class ParserTests
     {
+        protected const string TestFileDirectory = "TestFiles";
+
+        protected Assembly GetAssembly()
+        {
+            var assembly = typeof(ParserTests).GetTypeInfo().Assembly;
+            return assembly;
+        }
+
+        protected string GetResourceName(Assembly assembly, string fileName)
+        {
+            var assemblyFullName = assembly.GetName().Name;
+            var resource = $"{assemblyFullName}.{TestFileDirectory}.{fileName}";
+            return resource;
+        }
+
+        protected void AssertCanReadEmbeddedResource(string fileName)
+        {
+            var assembly = GetAssembly();
+            var resource = GetResourceName(assembly, fileName);
+            using (Stream stream = assembly.GetManifestResourceStream(resource))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var line = reader.ReadLine();
+                Assert.True(
+                    line != null,
+                    $"Could not read anything from embedded resource '{resource}'."
+                    );
+            }
+        }
+
         public class MMCacheSimpleExampleTests : ParserTests
         {
             private const string _fileName = "MMCacheSimpleExample.cfg";
 
-            
+            [Fact]
+            public void CanReadTestFile()
+            {
+                AssertCanReadEmbeddedResource(_fileName);
+            }
         }
     }
 }
