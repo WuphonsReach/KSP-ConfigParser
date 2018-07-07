a190a90ddb2697535c8daa104e2359713ee83bce
Unknown <wuphonsreach@gmail.com>
2018-07-07 18:45:05 -0400

    creating the Parser class

--- src/parse/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace parse
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/parse/Parser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using parse.Models;
+
+namespace parse
+{
+    public class Parser
+    {
+        public ConfigFile ParseConfigFile(string filePath)
+        {
+            var result = new ConfigFile();
+            return result;
+        }
+    }
+}
