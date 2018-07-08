9f4d4dc07135518c12b6bb30cca6c66595e268ba
Unknown <wuphonsreach@gmail.com>
2018-07-08 12:53:17 -0400

    add tests to look at Attributes

--- test/parse.Tests/TestFileTests/CurlyBraceOnSameLineTests.cs
+++ test/parse.Tests/TestFileTests/CurlyBraceOnSameLineTests.cs
@@ -22,6 +22,22 @@ namespace parse.Tests.TestFileTests
         }
 
         [Fact]
+        public void Part_has_correct_InputLines_count()
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            Assert.Equal(5, firstNode.InputLines.Count);
+        }
+
+        [Theory]
+        [InlineData("name", "Example")]
+        public void Part_has_attribute_and_value(string attributeName, string expectedValue)
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            var nameAttribute = firstNode.Attributes.First(x => x.Name == attributeName);
+            Assert.Equal(expectedValue, nameAttribute.Value);
+        }
+
+        [Fact]
         public void Part_node_has_resource_node()
         {
             var firstNode = _configFile.RootNode.Nodes.First();
@@ -30,11 +46,38 @@ namespace parse.Tests.TestFileTests
         }
 
         [Fact]
+        public void Part_resource_has_correct_InputLines_count()
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            var resourceNode = firstNode.Nodes.First(x => x.Type == NodeType.Resource);
+            Assert.Equal(5, resourceNode.InputLines.Count);
+        }
+
+        [Theory]
+        [InlineData("name", "LiquidFuel")]
+        [InlineData("amount", "400")]
+        public void Part_resource_has_attribute_and_value(string attributeName, string expectedValue)
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            var resourceNode = firstNode.Nodes.First(x => x.Type == NodeType.Resource);
+            var nameAttribute = resourceNode.Attributes.First(x => x.Name == attributeName);
+            Assert.Equal(expectedValue, nameAttribute.Value);
+        }
+
+        [Fact]
         public void Part_node_has_module_node()
         {
             var firstNode = _configFile.RootNode.Nodes.First();
-            var resourceNode = firstNode.Nodes.First(x => x.Type == NodeType.Module);
-            Assert.NotNull(resourceNode);
+            var moduleNode = firstNode.Nodes.First(x => x.Type == NodeType.Module);
+            Assert.NotNull(moduleNode);
+        }
+
+        [Fact]
+        public void Part_module_has_correct_InputLines_count()
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            var moduleNode = firstNode.Nodes.First(x => x.Type == NodeType.Module);
+            Assert.Equal(4, moduleNode.InputLines.Count);
         }
     }
 }
