2df9915621413e1e4bf11e1a7bc39bec9343bb6a
Unknown <wuphonsreach@gmail.com>
2018-07-08 07:55:32 -0400

    setting up tests for splitting a data element

---
+++ test/parse.Tests/Extensions/InputLineExtensionsTests.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using parse.Extensions;
+using parse.Models;
+using Xunit;
+
+namespace parse.Tests.Extensions
+{
+    public class InputLineExtensionsTests
+    {
+        [Theory]
+        [InlineData("x", "x")]
+        [InlineData("x{", "x", "{")]
+        [InlineData("PART { MODULE { X = 3 } } // TEST COMMENT", "PART", "{", "MODULE", "{", "X = 3", "}", "}")]
+        public void SplitLineDataOnBraces_gives_correct_output(string input, params string[] expected)
+        {
+            var inputLine = new InputLine { Data = input };
+            var results = inputLine.SplitLineDataOnBraces().ToArray();
+
+            Assert.Equal(expected.Count(), results.Count());
+            for (var i = 0; i < expected.Count(); i++)
+            {
+                Assert.Equal(expected[i], results[i].Data);
+            }
+        }
+    }
+}
