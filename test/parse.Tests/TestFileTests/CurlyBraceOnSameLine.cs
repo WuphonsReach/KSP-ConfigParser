4b6daa8e3be685d266453bd582f401ffc06c4148
Unknown <wuphonsreach@gmail.com>
2018-07-07 21:22:45 -0400

    example where curly braces are on same line

---
+++ test/parse.Tests/TestFileTests/CurlyBraceOnSameLine.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using parse.Models;
+using Xunit;
+
+namespace parse.Tests.TestFileTests
+{
+    public class CurlyBraceOnSameLine : ParserTests
+    {
+        private const string _fileName = TestFileNames.CurlyBraceOnSameLine;
+        private readonly ConfigFile _configFile;
+
+        public CurlyBraceOnSameLine()
+        {
+            _configFile = GetParsedConfigFile(_fileName);
+        }
+
+        [Fact]
+        public void First_top_level_node_must_be_part()
+        {
+            var firstNode = _configFile.Nodes.First();
+            Assert.Equal(NodeType.Part, firstNode.Type);
+        }
+
+        [Fact]
+        public void Part_node_has_resource_node()
+        {
+            var firstNode = _configFile.Nodes.First();
+            var resourceNode = firstNode.Nodes.First(x => x.Type == NodeType.Resource);
+            Assert.NotNull(resourceNode);
+        }
+
+        [Fact]
+        public void Part_node_has_module_node()
+        {
+            var firstNode = _configFile.Nodes.First();
+            var resourceNode = firstNode.Nodes.First(x => x.Type == NodeType.Module);
+            Assert.NotNull(resourceNode);
+        }
+    }
+}
