d9403a48ff36293c56b41b24f38b7a44320e5a6a
Unknown <wuphonsreach@gmail.com>
2018-07-07 21:06:01 -0400

    define some commonly used node types

---
+++ src/parse/NodeType.cs
@@ -0,0 +1,21 @@
+namespace parse
+{
+    public enum NodeType
+    {
+        Unknown, // default
+
+        // Squad
+        Part,
+        Model,
+        Internal,
+        Module,
+        Resource,
+        Effect,
+        Result,
+        Prop,
+
+
+        // ModuleManager
+        UrlConfig, 
+    }
+}
