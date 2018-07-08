18ed98014ecf3e2f9b8b1e91feca79271d05ec45
Unknown <wuphonsreach@gmail.com>
2018-07-07 21:06:13 -0400

    Capture the node type and the string identifier

--- src/parse/Models/Node.cs
+++ src/parse/Models/Node.cs
@@ -4,6 +4,8 @@ namespace parse.Models
 {
     public class Node
     {
+        public NodeType Type { get; set; }
+        public string TypeIdentifier { get; set; }
         public ICollection<Node> Nodes { get; set; } = new List<Node>();
         public ICollection<Attribute> Attributes { get; set; }
     }
