b8ee13655352aedda0a05f27033e6f1cdd10e353
Unknown <wuphonsreach@gmail.com>
2018-07-08 13:49:20 -0400

    add Parent property

--- src/parse/Models/ConfigNode.cs
+++ src/parse/Models/ConfigNode.cs
@@ -4,6 +4,7 @@ namespace parse.Models
 {
     public class ConfigNode
     {
+        public ConfigNode Parent { get; set; }
         public NodeType Type { get; set; }
         public string TypeIdentifier { get; set; }
         public ICollection<ConfigNode> Nodes { get; set; } = new List<ConfigNode>();
