d50d296cabd0206f34176294b7108831215486f8
Unknown <wuphonsreach@gmail.com>
2018-07-08 11:24:56 -0400

    refactor to start collecting lines into blocks

--- src/parse/Parser.cs
+++ src/parse/Parser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using parse.Extensions;
 using parse.Models;
 
@@ -10,11 +11,89 @@ namespace parse
     {
         public ConfigFile ParseConfigFile(string filePath, Stream stream)
         {
+            var fileLines = ReadStreamIntoInputLines(filePath, stream);
             var result = new ConfigFile
             {
                 FilePath = filePath,
             };
 
+            result.Nodes = ParseInputLinesIntoNodes(fileLines);
+
+            return result;
+        }
+
+        private ICollection<ConfigNode> ParseInputLinesIntoNodes(IList<InputLine> fileLines)
+        {
+            var results = new List<ConfigNode>();
+            if (fileLines == null) return results;
+
+            IdentifyBlocks(fileLines);
+            results = ParseBlocksIntoNodes(fileLines);
+            return results;
+        }
+
+        private List<ConfigNode> ParseBlocksIntoNodes(IList<InputLine> fileLines)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IdentifyBlocks(IList<InputLine> fileLines)
+        {
+            int blockId = 0;
+            int depth = 0;
+            for(var i = 0; i <= fileLines.Count; i++)
+            {
+                if (IsOpeningBrace(fileLines[i].Data))
+                {
+                    blockId++;
+                    depth++;
+
+                    MarkBlockIdentifier(i, fileLines, blockId, depth);
+                }
+
+                if (IsClosingBrace(fileLines[i].Data))
+                {
+                    depth--;
+                }
+
+                fileLines[i].BlockId = blockId;
+                fileLines[i].BlockDepth = depth;
+            }
+        }
+
+        ///<summary>Walk backwards in previous block/depth to find block identifier</summary>
+        private void MarkBlockIdentifier(int openingBraceIndex, IList<InputLine> fileLines, int blockId, int depth)
+        {
+            for (var i = openingBraceIndex - 1; i >= 0; i--)
+            {
+                if (fileLines[i].BlockDepth != depth - 1) return;
+                if (fileLines[i].BlockId != blockId - 1) return;
+
+                fileLines[i].BlockDepth = depth;
+                fileLines[i].BlockId = blockId;
+
+                const string blockIdentifierRegexPattern = @"^[a-zA-Z0-9_-]+$";
+                Regex blockIdentifierRegex = new Regex(blockIdentifierRegexPattern);
+                if (blockIdentifierRegex.Match(fileLines[i].Data).Success)
+                {
+                    return;
+                }
+            }
+        }
+
+        private bool IsOpeningBrace(string line)
+        {
+            return (line == Constants.OpeningBrace);
+        }
+
+        private bool IsClosingBrace(string line)
+        {
+            return (line == Constants.ClosingBrace);
+        }
+
+        private IList<InputLine> ReadStreamIntoInputLines(string filePath, Stream stream)
+        {
+            int rawLineCounter = 1;
             var lines = new List<InputLine>();
 
             using (StreamReader reader = new StreamReader(stream))
@@ -24,7 +103,7 @@ namespace parse
                     var input = reader.ReadLine();
                     if (input != null)
                     {
-                        var line = input.ParseLine();
+                        var line = input.ParseLine(rawLineCounter);
                         if (!string.IsNullOrWhiteSpace(line.Data))
                         {
                             // We have an interesting line with data
@@ -32,10 +111,11 @@ namespace parse
                             lines.AddRange(candidates);
                         }
                     }
+                    rawLineCounter++;
                 }
             }
 
-            return result;
+            return lines;        
         }
     }
 }
