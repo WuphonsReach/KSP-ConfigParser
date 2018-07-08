b34a929612aa0e71afda52bfdcb813b6e5843738
Unknown <wuphonsreach@gmail.com>
2018-07-08 13:35:33 -0400

    add tests for attribute values

--- test/parse.Tests/TestFileTests/SimplePartTests.cs
+++ test/parse.Tests/TestFileTests/SimplePartTests.cs
@@ -20,5 +20,37 @@ namespace parse.Tests.TestFileTests
             var result = _configFile.RootNode.Nodes.First();
             Assert.Equal(NodeType.Part, result.Type);
         }
+
+        [Theory]
+        [InlineData("name", "HighGainAntenna")]
+        [InlineData("module", "Part")]
+        [InlineData("rescaleFactor", "1")]
+        [InlineData("maxTemp", "2000")]
+        [InlineData("bulkheadProfiles", "srf")]
+        public void Part_node_has_attribute_values(string name, string value)
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            var attribute = firstNode.AttributeDefinitions.First(x => x.Name == name);
+            Assert.Equal(value, attribute.Value);
+        }
+
+        [Fact]
+        public void Part_node_has_model_node()
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            var node = firstNode.Nodes.First(x => x.Type == NodeType.Model);
+            Assert.NotNull(node);
+        }
+
+        [Theory]
+        [InlineData("model", "Squad/Parts/Misc/ABC/HighGainAntenna")]
+        public void Part_model_node_has_attribute_values(string name, string value)
+        {
+            var firstNode = _configFile.RootNode.Nodes.First();
+            var node = firstNode.Nodes.First(x => x.Type == NodeType.Model);
+            var attribute = node.AttributeDefinitions.First(x => x.Name == name);
+            Assert.Equal(value, attribute.Value);
+        }
+
     }
 }
