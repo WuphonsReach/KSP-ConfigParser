adb5cd0c666ecde5245c920441bcb8176dc3bb5c
Unknown <wuphonsreach@gmail.com>
2018-07-08 20:54:50 -0400

    this now parses properly

--- test/parse.Tests/TestFileTests/MMCacheSimpleExampleTests.cs
+++ test/parse.Tests/TestFileTests/MMCacheSimpleExampleTests.cs
@@ -21,9 +21,17 @@ namespace parse.Tests.TestFileTests
         public void Can_find_activeTextureManagerConfig_node()
         {
             var nodes = _configFile.RootNode.Descendants();
+
             var node = nodes.First(x => x.TypeIdentifier == activeTextureManagerConfig);
+            Assert.Equal(2, node.InputLines.First().BlockDepth);
+            Assert.Equal(2, node.AttributeDefinitions.Count);
+
             var overridesNode = node.Nodes.First(x => x.TypeIdentifier == "OVERRIDES");
+            Assert.Equal(3, overridesNode.InputLines.First().BlockDepth);
+            Assert.Equal(0, overridesNode.AttributeDefinitions.Count);
+
             var scanSatNode = overridesNode.Nodes.First(x => x.TypeIdentifier == "SCANsat/Icons/.*");
+            Assert.Equal(4, scanSatNode.InputLines.First().BlockDepth);
             Assert.Equal(5, scanSatNode.AttributeDefinitions.Count);
         }
     }
