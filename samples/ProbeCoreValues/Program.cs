376eed3d17786e8402c3345f65c5036f56b35517
Unknown <wuphonsreach@gmail.com>
2018-07-20 12:33:03 -0400

    $ dotnet new console

---
+++ samples/ProbeCoreValues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProbeCoreValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
